9967efad69f9267b82d9671a275449ce17ff9e6e
yanghao <283403891@qq.com>
2018-03-17 22:28:22 +0800

    Fetcher精简

--- python-server/handlers/fetcher.py
+++ python-server/handlers/fetcher.py
@@ -6,11 +6,11 @@
 
 import json
 import traceback
-
 from tornado.gen import coroutine, Return
 from tornado.log import app_log
 from tornado.httpclient import AsyncHTTPClient, HTTPRequest, HTTPError
 from tornado import escape
+
 
 class Fetcher(object):
     def __init__(self, host):
@@ -71,86 +71,24 @@ class Fetcher(object):
         raise Return(results)
 
 
-
-
 LOCALHOST = '10.0.0.161'
 
-MICROCLOUD_HOST = '10.0.0.119'
-
-MICROCLOUD_HOST_S = {
-    '10.0.0.118': '10.0.0.118',
-    '10.0.0.119': '10.0.0.119',
-    '10.0.0.252': '10.0.0.252',
-}
-
-
-
-
 CC_MODULES = {
-    "compute": (LOCALHOST, 30001),
-    "network": (LOCALHOST, 30002), 
-    "api": (LOCALHOST, 30003),
-    "webservices": (LOCALHOST, 30004)
+    "Compute": (LOCALHOST, 30001),
+    "Network": (LOCALHOST, 30002),
+    "ApiModule": (LOCALHOST, 30003),
+    "WebServices": (LOCALHOST, 30004)
 }
 
 
-def get_module_url(PART, module):
-    _url, _port = PART[module]
+def get_module_url(part, module):
+    _url, _port = part[module]
     return "http://%s:%s/api/v1" %(_url, _port)
 
 
-def get_microcloud_url(handler, module):
-    remote_ip = handler.request.remote_ip
-    fetch_host = MICROCLOUD_HOST_S.get(remote_ip, MICROCLOUD_HOST)
+cc_compute_fetcher = Fetcher(get_module_url(CC_MODULES, "Compute"))
+cc_network_fetcher = Fetcher(get_module_url(CC_MODULES, "Network"))
+cc_api_fetcher = Fetcher(get_module_url(CC_MODULES, "ApiModule"))
 
-    MICROCLOUD_MODULES = {
-        "webservices": (fetch_host, 30001),
-        "compute": (fetch_host, 30002),
-        "network": (fetch_host, 30003),
-        "storage": (fetch_host, 30004),
-        "images": (fetch_host, 30005)
-    }
-    _url, _port = MICROCLOUD_MODULES[module]
-    return "http://%s:%s/api/v1" %(_url, _port)
-
-
-
-# CC_MODULES 固定来着161
+# # use
 # from handlers.fetcher import cc_compute_fetcher, cc_network_fetcher
-cc_compute_fetcher = Fetcher(get_module_url(CC_MODULES, "compute"))
-cc_network_fetcher = Fetcher(get_module_url(CC_MODULES, "network"))
-cc_api_fetcher = Fetcher(get_module_url(CC_MODULES, "api"))
-
-
-
-# MICROCLOUD_MODULES 要根据请求的主机分发请求
-# 在用到的文件夹中：在该handler里面使用
-# 还是写到BaseProxyCCHandler作为内部属性使用吧！
-# 这样还是没成功额
-
-"""
-from handlers.fetcher import Fetcher, get_microcloud_url
-
-webservices_fetcher =  Fetcher(get_microcloud_url(self, 'webservices'))
-compute_fetcher = Fetcher(get_microcloud_url(self, "compute"))
-network_fetcher = Fetcher(get_microcloud_url(self, "network"))
-storage_fetcher = Fetcher(get_microcloud_url(self, "storage"))
-images_fetcher = Fetcher(get_microcloud_url(self, "images"))
-
-"""
-
-
-def webservices_fetcher(handler):
-    return Fetcher(get_microcloud_url(handler, 'webservices'))
-
-def compute_fetcher(handler):
-    return Fetcher(get_microcloud_url(handler, 'compute'))
-
-def network_fetcher(handler):
-    return Fetcher(get_microcloud_url(handler, 'network'))
-
-def storage_fetcher(handler):
-    return Fetcher(get_microcloud_url(handler, 'storage'))
-
-def images_fetcher(handler):
-    return Fetcher(get_microcloud_url(handler, 'images'))
