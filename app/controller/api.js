c9eca9273207c59f98a7d296e5c9943b06d2edfb
yanghao <283403891@qq.com>
2018-03-25 18:57:53 +0800

    param_children前端实现

--- app/controller/api.js
+++ app/controller/api.js
@@ -30,24 +30,34 @@ function apiController($scope, $http, $q, $routeParams, $location, $window, $anc
         $scope.authenticated = false;
     });
 
-    var param_demo = {
-        "name": "param",
-        "default": "",
-        "required": "false",
-        "type_": "string",
-        "description": "",
-        "children": [],
-        "valid": true
+    var uuid = function () {
+        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+            var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+            return v.toString(16);
+        });
     };
-
-    var json_sub_param_demo = {
-        "name": "json:param",
-        "default": "",
-        "required": "false",
-        "type_": "string",
-        "description": "",
-        "children": [],
-        "valid": false
+    var param_equal = function(param1, param2) {
+        return param1.uuid == param2.uuid;
+    };
+    var param_demo = function() {
+        return {
+            "name": "param",
+            "default": "",
+            "required": "false",
+            "type_": "string",
+            "description": "",
+            "uuid": uuid(),
+            "children": [],
+            "parent": null, //parent ==null, valid:true,; parent!=null, valid:false
+            "valid": true
+        }
+    };
+    var json_sub_param_demo = function(parent_uuid) {
+        var param = param_demo();
+        param.valid = false;
+        param.name = "json:param";
+        param.parent = parent_uuid;
+        return param;
     };
     var empty = {
         "_id": "-1",
@@ -56,26 +66,10 @@ function apiController($scope, $http, $q, $routeParams, $location, $window, $anc
         "method": "GET",
         "description": "接口描述",
         "paramsList": [
-            {
-                "name": "id",
-                "default": "",
-                "required": "false",
-                "type_": "int",
-                "description": "",
-                "children": [],
-                "valid": true
-            }
+            param_demo()
         ],
         "responseList":[
-            {
-                "name": "id",
-                "default": "1",
-                "required": "false",
-                "type_": "int",
-                "description": "id.",
-                "children": [],
-                "valid": true
-            }
+            param_demo()
         ],
         "paramsListVirtual": [],
         "responseListVirtual": [],
@@ -90,6 +84,7 @@ function apiController($scope, $http, $q, $routeParams, $location, $window, $anc
     $scope.METHODS = [
         "GET", "POST", "PUT", "DELETE"
     ];
+
 
     $scope.add = function(){
         $scope.current = angular.copy(empty);
@@ -280,30 +275,79 @@ function apiController($scope, $http, $q, $routeParams, $location, $window, $anc
         $location.hash(old);
     };
 
-    $scope.remove_param = function(collection, item){
-        //  <button ng-show="show_edit" class="btn btn-danger btn-xs pull-right" style="margin-right: 5px;" ng-click="remove(apiList, item); save_me();">delete</button>
-        if(confirm("确认删除param?")) {
-            for(var i = 0; i < collection.length; i++) {
-                if(collection[i] == item) {
-                    collection.splice(i, 1);
-                }
+    $scope.add_param = function(collection){
+        var param = param_demo();
+        collection.push(param);
+    };
+
+    $scope.add_json_param_children = function(collection, item){
+        //增加只能末位添加的检查
+        var pos = collection.length - 1;
+        do {
+            var t = collection[pos];
+            if(!t.parent) {  // !t.parent && t.type_ =='json'
+                break;
+            }
+            pos -= 1;
+        } while (pos >= 0);
+        //console.log(collection.length);
+        //console.log(pos);
+        //console.log(collection.indexOf(item));
+        if(collection.indexOf(item) != pos) {
+            alert('只能在末位json param新增子项');
+            return;
+        }
+        var sub_param = json_sub_param_demo(item.uuid);
+        item.children.push(sub_param);
+        collection.push(sub_param);
+        // 上面push的是同一个对象，所以同时更新；
+        // 但是如果是用下面这种方式，肯定就不同时更新了
+        // var copy = angular.copy(sub_param);
+        // collection.push(copy);
+    };
+
+    var remove_i = function(collection, item){
+        for(var i = 0; i < collection.length; i++) {
+            if(collection[i] == item) {
+                collection.splice(i, 1);
+                break;
             }
         }
     };
 
+    $scope.remove_param = function(collection, item){
 
-    $scope.add_json_param_children = function(item, collection_virtual){
-        var copy = angular.copy(param_demo);
-        item.children.push(copy);
-        var copy_2 = angular.copy(json_sub_param_demo);
-        collection_virtual.push(copy_2);
-    };
 
-    $scope.remove_json_param_children = function(collection, item){
         if(confirm("确认删除param?")) {
+            if (item.type_ == 'json' && item.children.length) {
+                if(confirm("该param有子元素，请再次确认删除param?")) {
+                    // 1.
+                    remove_i(collection, item);
+                    // 2.
+                    for(var i = 0; i < collection.length; i++) {
+                        if(collection[i].parent == item.uuid) {
+                            remove_i(collection, collection[i]);
+                            i--;
+                            //这种用collection.length作为循环条件，但是在内部因为splice的操作，导致循环条件动态变化，并不符合真正的编程科学...
+                        }
+                    }
+                }
+            } else {
+                remove_i(collection, item);
+            }
+
+
+        }
+    };
+    $scope.remove_json_param_children = function(collection, item){
+        if(confirm("确认删除param??")) {
+            // 1.
+            remove_i(collection, item);
+            // 2.
             for(var i = 0; i < collection.length; i++) {
-                if(collection[i] == item) {
-                    collection.splice(i, 1);
+                if(collection[i].uuid == item.parent) {
+                    remove_i(collection[i].children, item);
+                    break;
                 }
             }
         }
@@ -321,14 +365,17 @@ function apiController($scope, $http, $q, $routeParams, $location, $window, $anc
                 });
         }
     };
-    $scope.add_param = function(collection){
-        var copy = angular.copy(param_demo);
-        collection.push(copy);
-    };
+
 
     $scope.back = function(){
         $scope.scrollTo("top");
     };
 
+    $scope.log = function(current_item, current_param){
+        console.log(current_item);
+        if(current_param) {
+            console.log(current_param);
+        }
+    };
 }
 
