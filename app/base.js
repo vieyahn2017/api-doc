a0641287d467f598837ceb1dee9f93fdcaa3d5e6
yanghao <283403891@qq.com>
2018-03-28 23:23:26 +0800

    is_authenticated 增加一种简单的验证模式

--- app/base.js
+++ app/base.js
@@ -3,3 +3,27 @@ function get_base_url() {
     base_url = host + "/api/v1/";
     return base_url;
 }
+
+function is_authenticated() {
+    return true;
+}
+
+/*
+
+function is_authenticated($scope, $http) {
+
+    $http.get(get_base_url() + "m/api/authenticated").success(function(data){
+        if(data.code == 1) {
+            $scope.authenticated = true;
+        } else if (data.code == -1) {
+            $scope.authenticated = false;
+        }
+        console.log($scope.authenticated);
+    }).error(function(){
+        $scope.authenticated = false;
+    });
+
+    return $scope.authenticated;
+}
+
+*/
